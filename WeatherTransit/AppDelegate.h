6c0815375497c31c1bda27a4e50d42134a9ce015
Michelle Burke <michele.burke01@gmail.com>
2017-04-18 17:48:35 -0500

    Initial Commit

---
+++ WeatherTransit/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  WeatherTransit
+//
+//  Created by Michelle Burke on 4/18/17.
+//  Copyright © 2017 BurkeLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ WeatherTransit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeatherTransit
+//
+//  Created by Michelle Burke on 4/18/17.
+//  Copyright © 2017 BurkeLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
